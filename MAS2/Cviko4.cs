8bed1496b4ee81d89bb405ec727d04b8379b362e
Michal Duba <m.duba.work@gmail.com>
2025-10-21 20:02:01 +0200

    Cviko 4 done

--- MAS2/Cviko4.cs
+++ MAS2/Cviko4.cs
@@ -11,15 +11,17 @@ namespace MAS2
         public static void Main(string[] args)
         {
             int nodeCount = 1500;
-
+            double q = 0.05;
             var linkSelectionMatrix = NetworkGenerator.GenerateLinkSelectionModel(nodeCount);
             NetworkGenerator.SaveMatrixAsEdgeList(linkSelectionMatrix, "link_selection_1500.txt");
-
+            var linkSelectionMatrixIL = NetworkGenerator.GenerateLinkSelectionModelWithInternalLinks(nodeCount, q);
+            NetworkGenerator.SaveMatrixAsEdgeList(linkSelectionMatrixIL, "link_selection_1500_with_internal_links.txt");
 
             double p = 0.5;
             var copyingModelMatrix = NetworkGenerator.GenerateCopyingModel(nodeCount, p);
             NetworkGenerator.SaveMatrixAsEdgeList(copyingModelMatrix, "copying_model_1500.txt");
-
+            var copyingModelMatrixIL = NetworkGenerator.GenerateCopyingModelWithInternalLinks(nodeCount, p, q);
+            NetworkGenerator.SaveMatrixAsEdgeList(copyingModelMatrixIL, "copying_model_1500_with_internal_links.txt");
             Console.WriteLine("Networks generated and saved.");
         }
     }
--- MAS2/NetworkGenerator.cs
+++ MAS2/NetworkGenerator.cs
@@ -101,6 +101,146 @@ namespace MAS2
         }
 
         /// <summary>
+        /// Generates a network using the Link Selection Model with the Internal-Link principle.
+        /// At each new-node step there is a probability q to add an extra internal link between two
+        /// existing nodes (excluding the newly added node).
+        /// </summary>
+        /// <param name="nodeCount">Total number of nodes in the network.</param>
+        /// <param name="q">Probability of adding an internal link at each step (0 <= q <= 1).</param>
+        /// <returns>DokSparseMatrix<int> representing the adjacency matrix.</returns>
+        public static DokSparseMatrix<int> GenerateLinkSelectionModelWithInternalLinks(int nodeCount, double q)
+        {
+            if (nodeCount < 2)
+                throw new ArgumentException("Network must have at least 2 nodes.");
+            if (q < 0 || q > 1)
+                throw new ArgumentException("Probability q must be between 0 and 1.");
+
+            var matrix = new DokSparseMatrix<int>(nodeCount, nodeCount);
+            var links = new List<(int, int)>();
+            var rand = new Random();
+
+            // Start with two nodes connected
+            matrix[0, 1] = 1;
+            matrix[1, 0] = 1;
+            links.Add((0, 1));
+
+            for (int newNode = 2; newNode < nodeCount; newNode++)
+            {
+                // Select a link at random
+                var selectedLink = links[rand.Next(links.Count)];
+                // Randomly pick one endpoint
+                int endpoint = rand.Next(2) == 0 ? selectedLink.Item1 : selectedLink.Item2;
+                // Connect new node to the selected endpoint
+                matrix[newNode, endpoint] = 1;
+                matrix[endpoint, newNode] = 1;
+                // Add new link to the list
+                links.Add((newNode, endpoint));
+
+                // With probability q add an internal link between two existing nodes (excluding newNode)
+                if (rand.NextDouble() < q && newNode > 1)
+                {
+                    int a = rand.Next(newNode);
+                    int b = rand.Next(newNode);
+                    // ensure distinct
+                    int tries = 0;
+                    while (b == a && tries < 5)
+                    {
+                        b = rand.Next(newNode);
+                        tries++;
+                    }
+                    if (a != b && matrix[a, b] == 0)
+                    {
+                        matrix[a, b] = 1;
+                        matrix[b, a] = 1;
+                        links.Add((a, b));
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Generates a network using the Copying Model with the Internal-Link principle.
+        /// At each new-node step there is a probability q to add an extra internal link between two
+        /// existing nodes (excluding the newly added node).
+        /// </summary>
+        /// <param name="nodeCount">Total number of nodes in the network.</param>
+        /// <param name="p">Probability of random connection (0 <= p <= 1).</param>
+        /// <param name="q">Probability of adding an internal link at each step (0 <= q <= 1).</param>
+        /// <returns>DokSparseMatrix<int> representing the adjacency matrix.</returns>
+        public static DokSparseMatrix<int> GenerateCopyingModelWithInternalLinks(int nodeCount, double p, double q)
+        {
+            if (nodeCount < 2)
+                throw new ArgumentException("Network must have at least 2 nodes.");
+            if (p < 0 || p > 1)
+                throw new ArgumentException("Probability p must be between 0 and 1.");
+            if (q < 0 || q > 1)
+                throw new ArgumentException("Probability q must be between 0 and 1.");
+
+            var matrix = new DokSparseMatrix<int>(nodeCount, nodeCount);
+            var rand = new Random();
+
+            // Start with two nodes connected
+            matrix[0, 1] = 1;
+            matrix[1, 0] = 1;
+
+            for (int newNode = 2; newNode < nodeCount; newNode++)
+            {
+                // Select a node u at random from existing nodes
+                int u = rand.Next(newNode);
+                if (rand.NextDouble() < p)
+                {
+                    // With probability p, connect to u
+                    matrix[newNode, u] = 1;
+                    matrix[u, newNode] = 1;
+                }
+                else
+                {
+                    // With probability 1-p, copy one of u's outgoing links
+                    var outgoing = new List<int>();
+                    for (int target = 0; target < newNode; target++)
+                    {
+                        if (matrix[u, target] != 0)
+                            outgoing.Add(target);
+                    }
+                    if (outgoing.Count > 0)
+                    {
+                        int target = outgoing[rand.Next(outgoing.Count)];
+                        matrix[newNode, target] = 1;
+                        matrix[target, newNode] = 1;
+                    }
+                    else
+                    {
+                        // If u has no outgoing links, fallback to random connection
+                        matrix[newNode, u] = 1;
+                        matrix[u, newNode] = 1;
+                    }
+                }
+
+                // With probability q add an internal link between two existing nodes (excluding newNode)
+                if (rand.NextDouble() < q && newNode > 1)
+                {
+                    int a = rand.Next(newNode);
+                    int b = rand.Next(newNode);
+                    int tries = 0;
+                    while (b == a && tries < 5)
+                    {
+                        b = rand.Next(newNode);
+                        tries++;
+                    }
+                    if (a != b && matrix[a, b] == 0)
+                    {
+                        matrix[a, b] = 1;
+                        matrix[b, a] = 1;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
         /// Saves a DokSparseMatrix<int> as an edge list to a file.
         /// </summary>
         /// <param name="matrix">The matrix to save.</param>
